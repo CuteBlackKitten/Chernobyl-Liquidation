f78ba2c551541efa0ecce3bc7401cb33d0b2bb7b
Alex <tishchenkoar@gmail.com>
2026-08-30 21:48:18 +0200

    Fix issues

--- Assets/Scripts/OwnScripts/SceneController.cs
+++ Assets/Scripts/OwnScripts/SceneController.cs
@@ -12,28 +12,33 @@ public class SceneController : MonoBehaviour
     {
         SceneManager.LoadScene(3);
         GlobalVariableHolder.ChangeCurrentLevel(3);
+        Time.timeScale = 1f;
     }
 
     public void EnterGameFF()
     {
         SceneManager.LoadScene(4);
         GlobalVariableHolder.ChangeCurrentLevel(1);
+        Time.timeScale = 1f;
     }
 
     public void EnterTheMenu()
     {
         SceneManager.LoadScene(0);
         GlobalVariableHolder.ChangeCurrentLevel(0);
+        Time.timeScale = 1f;
     }
     public void Retry()
     {
         if (GlobalVariableHolder.ReturnCurrentLevel() == 1)
         {
-            SceneManager.LoadScene(5);
+            SceneManager.LoadScene(4);
+            Time.timeScale = 1f;
         }
         if (GlobalVariableHolder.ReturnCurrentLevel() == 3)
         {
             SceneManager.LoadScene(3);
+            Time.timeScale = 1f;
         }
     }
 }
